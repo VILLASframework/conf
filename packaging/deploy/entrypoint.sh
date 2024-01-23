cd6131be19cf00bca884676f6ebb446685c95bfc
Jonas Schneider <jonas.max.schneider@gmail.com>
2024-01-23 17:19:39 +0100

    auto add deploy villas-conf-node to deploy container

--- packaging/deploy/entrypoint.sh
+++ packaging/deploy/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-/usr/bin/node node_modules/node-red/red.js --userDir /data $FLOWS &
-
-cd /data
 npm i /villas-conf-node
 
-sleep infinity
+node node_modules/node-red/red.js --userDir /data $FLOWS
