fc764acf260a7c71998ca1302d14cbd2ebbc6f8b
iripiri <iris.m.koester@gmail.com>
2026-09-16 13:13:02 +0200

    made average hook work
    Signed-off-by: iripiri <iris.m.koester@gmail.com>

--- villas-conf-node/hooks/average.js
+++ villas-conf-node/hooks/average.js
@@ -3,65 +3,29 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         var node = this;
 
-        node.offset = config.offset || 0;
-        node.signals = config.signals || [];
-        node.enabled = config.enabled !== false;
-        node.priority = config.priority || 99;
+        const builder = this.context().flow.get("builder");
+
+        const hookConfig = {
+            type: "average",
+            signals: [],
+            ...(config.offset != null && { offset: Number(config.offset) }),
+            ...(config.priority != null && { priority: Number(config.priority) }),
+        }
 
         node.on('input', function(msg) {
-            if (!node.enabled) {
-                node.send(msg);
-                return;
+            const wires = this.wires.flat();
+            hookConfig.signals = [];
+
+            builder.addHook2(this.id, hookConfig, msg, wires);
+
+            msg.payload = {
+                ...msg.payload,
+                origin: this.hooktype,
+                originId: this.id,
+                trace: [...(msg.payload.trace || []), this.id]
             }
+            node.send(msg);
 
-            if (!Array.isArray(node.signals) || node.signals.length === 0) {
-                node.error("No signals defined for averaging.");
-                return;
-            }
-
-            try {
-                let sum = 0;
-                let count = 0;
-
-                
-                for (let sig of node.signals) {
-                    let value = msg.payload[sig];
-
-                    if (typeof value === 'number') {
-                        sum += value;
-                        count++;
-                    }
-                    else {
-                        node.warn(`Signal "${sig}" is not a number. Skipping.`);
-                    }
-                }
-
-                if (count === 0) {
-                    node.error("No valid numeric signals found.");
-                    return;
-                }
-
-                let avg = sum / count;
-
-                
-                if (Array.isArray(msg.payload)) {
-                    
-                    msg.payload.splice(node.offset, 0, avg);
-                }
-                else if (typeof msg.payload === 'object') {
-                    
-                    msg.payload['average'] = avg;
-                }
-                else {
-                    node.error("Payload must be an object or array.");
-                    return;
-                }
-
-                node.send(msg);
-
-            } catch (err) {
-                node.error("Failed to calculate average: " + err.message);
-            }
         });
     }
 
