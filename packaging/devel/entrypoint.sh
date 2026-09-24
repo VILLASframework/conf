d25178e56282ecc994b0217da1dfe69c32e72f6e
iripiri <iris.m.koester@gmail.com>
2026-09-24 10:36:22 +0000

    adapt development docker settings to deploy settings
    Signed-off-by: iripiri <iris.m.koester@gmail.com>

--- packaging/devel/entrypoint.sh
+++ packaging/devel/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-npm i /data/villas-conf-node
+npm i /villas-conf-node
 npm i nodemon
 
 /usr/src/node-red/node_modules/.bin/nodemon \
-  --watch /data/villas-conf-node/ \
+  --watch /villas-conf-node/ \
   -e js,mjs,cjs,json,html \
   node_modules/node-red/red.js \
   --userDir /data \
-  --settings /data/villas-conf-node/settings.js \
+  --settings /villas-conf-node/settings.js \
   $FLOWS
